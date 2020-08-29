8d9b59bf5401446c9b3e6f750b8dd267e448d8f6
Oleg Stukalov <oleg.stukalov@mail.ru>
2020-08-29 13:01:38 +0300

    local var instead of global used

--- iterator.py
+++ iterator.py
@@ -9,7 +9,7 @@ CU_FILE = 'country_url.json'
 
 country_list = []
 wiki_list = []
-stop_counter = 0
+#stop_counter = 0
 
 class CountryURL:
     """
@@ -18,8 +18,7 @@ class CountryURL:
     """
 
     def __init__(self, url=JSON_RAW):
-        global stop_counter
-        stop_counter = 1
+        self.stop_counter = 1
         response = requests.get(url)
         print('Список стран будем брать отсюда:', url)
         # print(response, response.content)
@@ -66,9 +65,9 @@ class CountryURL:
         print('Список стран со ссылками Википедии успешно сохранен в локальный файл:', CU_FILE)
 
         #условие выхода из бесконечного цикла
-        global stop_counter
-        stop_counter += 1
-        if  stop_counter == len(country_list):
+        #global stop_counter
+        self.stop_counter += 1
+        if  self.stop_counter == len(country_list):
             raise StopIteration
 
 
